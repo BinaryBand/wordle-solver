893f188b5da6d1916b220ecd67a80d95db5a26e5
Shane <60277607+BinaryBand@users.noreply.github.com>
2023-03-20 14:26:28 -0700

    Added logic

--- final.js
+++ final.js
@@ -1,10 +1,10 @@
-javascript: (() => {
+javascript:(() => {
     const tiles = document.getElementsByClassName('Tile-module_tile__UWEHN');
 
     // { letter: number | null }
     const knowledge = {};
 
-    for (let i in tiles) {
+    for (let i = 0; i < tiles.length; i++) {
         const tile = tiles[i];
 
         if (!tile || !tile.getAttribute) {
@@ -12,20 +12,47 @@ javascript: (() => {
         }
 
         const state = tile.getAttribute('data-state');
-        const letter = tile.innerText;
+        const letter = tile.innerText.toLowerCase();
 
-        if (state === 'absent') {
-            knowledge[letter] = -2;
-        }
-        else if (state === 'present') {
+        if (state === 'absent' && !knowledge[letter]) {
             knowledge[letter] = -1;
         }
-        else if (state === 'correct') {
+        else if (state === 'present' && !knowledge[letter]) {
             knowledge[letter] = i % 5;
+        }
+        else if (state === 'correct') {
+            knowledge[letter] = i % 5 + 5;
         }
     }
 
-    for (let letter in knowledge) {
-        console.log(`${letter}: ${knowledge[letter]}`);
-    }
+    fetch('https://raw.githubusercontent.com/BinaryBand/wordle-solver/master/words.json')
+        .then((response) => response.json())
+        .then((words) => {
+            let letters = Object.keys(knowledge);
+
+            for (let i = 0; i < letters.length; i++) {
+                const letter = letters[i];
+
+                if (knowledge[letter] === -1) {
+                    words = words.filter((word) => {
+                        return !word.includes(letter);
+                    });
+                }
+                else if (knowledge[letter] < 4) {
+                    words = words.filter((word) => {
+                        const index = knowledge[letter];
+                        return word[index] !== letter && word.includes(letter);
+                    });
+                }
+                else {
+                    words = words.filter((word) => {
+                        const index = knowledge[letter] - 5;
+                        return word[index] === letter;
+                    });
+                }
+            }
+
+            alert(words[0]);
+        })
+        .catch(alert);
 })();
